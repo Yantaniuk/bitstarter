549d8a01e63a4e4f6707dbb16718d24c0384a65f
Yantaniuk <yury.antaniuk@gmail.com>
2025-06-10 16:55:59 +0200

    Fix documentation, typo, and enhance tests

--- test/graderTest.js
+++ test/graderTest.js
@@ -9,4 +9,11 @@ const expected = { h1: true, p: true, div: false };
 const result = grader.checkHtmlFile(testHtml, testChecks);
 
 assert.deepStrictEqual(result, expected);
+
+const missingHtml = path.join(__dirname, 'test-missing.html');
+const expectedMissing = { h1: false, p: false, div: false };
+const resultMissing = grader.checkHtmlFile(missingHtml, testChecks);
+
+assert.deepStrictEqual(resultMissing, expectedMissing);
+
 console.log('All tests passed.');
